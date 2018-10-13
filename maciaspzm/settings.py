afe62cde0792808d35363eccc3205ad07a2d2a12
maciaspzm <macias.pzm@gmail.com>
2018-10-13 14:59:48 -0500

    Sobraron espacios

--- maciaspzm/settings.py
+++ maciaspzm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9wc-kmsr%t!rz%=xw0)^fc2l904sf1*)ze%-qj-q!&wgt8##-k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
